1f45affd9852edc2f758474391bc162e38f950df
Du Henrique <eduardochenrique@gmail.com>
2016-12-07 10:09:02 -0200

    including everything

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -6,12 +6,18 @@ module.exports = function(app) {
   });
 
   app.post('/pagamentos/pagamento', function(req, res) {
-    console.log('requisição de teste');
+    console.log('req', req);
     var pagamento = req.body;
-    console.log('aqui', pagamento);
-    pagamento.status = 'CRIADO';
-    pagamento.data = new Date();
-    res.send(pagamento)
+
+    // pagamento.status = 'CRIADO';
+    // pagamento.data = new Date();
+
+    var connection = app.persistencia.connectionFactory();
+    var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+    pagamentoDao.salva(pagamento, function(error, result) {
+      res.json(pagamento);
+    });
   });
 
 }
