5972b61d8f4720c85b3539a43ad959316072f6fc
Du Henrique <eduardochenrique@gmail.com>
2016-12-07 10:35:01 -0200

    wip

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ var app = require('./config/custom-express')();
 app.listen(8888, function() {
   console.log('teste');
 });
+
+// https://cursos.alura.com.br/course/nodejs-avancado/task/20094 Persistindo o pagamento no banco
